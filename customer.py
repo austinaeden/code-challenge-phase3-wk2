831f338391efb1892322eb602fd9d582c9828085
austinaeden <austin.mbogo@student.moringaschool.com>
2023-08-29 14:00:18 +0300

    update

---
+++ customer.py
@@ -0,0 +1,37 @@
+class customer:
+    all_customers = []
+
+    def __init__(self, given_name=None,  family_name=None):
+        self._given_name = given_name
+        self._family_name = family_name
+        self._full_name = f"{given_name} {family_name}"
+        customer.all_customers.append(self)
+    
+    def given_name(self):
+        return self._given_name
+    
+    def set_given_name(self, first_name):
+        if isinstance(first_name, str):
+            self._given_name = first_name
+            self._full_name = f"{first_name} {self._family_name}"
+        else:
+            print (f"ERROR: {first_name} must be a string.")
+    
+    given_name = property(given_name, set_given_name)
+
+
+    def family_name(self):
+        return self._family_name
+    
+    def set_family_name(self, last_name):
+        if isinstance (last_name, str):
+            self._family_name = last_name
+            self._full_name = f"{self._given_name} {last_name}"
+        else:
+            print (f"ERROR: {last_name} must be a string.")
+    
+    family_name = property(family_name, set_family_name)
+
+    @classmethod
+    def all(cls):
+        return cls.all_customers
