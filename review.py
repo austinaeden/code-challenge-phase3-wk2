e4c13706dfa3418b0562e817c8325050dcc76cc9
austinaeden <austin.mbogo@student.moringaschool.com>
2023-08-29 14:42:35 +0300

    review

---
+++ review.py
@@ -0,0 +1,21 @@
+class review:
+    all_review = []
+
+    def __init__(self, customer: str, restaurant: str, rating: (int, float)):
+        self._customer = customer
+        self ._restaurant = restaurant
+        self._rating = rating
+        review.all_review.append(self)
+    
+    def rating(self):
+        return self._rating
+    
+    def customer(self):
+        return self._customer
+    
+    def restaurant(self):
+        return self._restaurant
+     
+    @classmethod
+    def all(cls):
+        return cls.all_review
